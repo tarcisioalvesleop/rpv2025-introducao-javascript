d5c5667981ebef0eba23ea119eb13991411b5816
tarcisioalvesleop@hotmail.com <tarcisioalvesleop@hotmail.com>
2025-12-11 20:46:31 -0300

    Atualizando o DOM

--- manipulando-a-DOM/script.js
+++ manipulando-a-DOM/script.js
@@ -1,3 +1,28 @@
-const paragrafo = document.createElement('p')
+const mainElement = document.createElement('main') //criação do elemento
+const h1Element = document.createElement('h1')
+const pElement = document.createElement('p')
+const spanElement = document.createElement('span')
 
-console.log()
+const bodyElement = document.querySelector('body')
+
+h1Element.innerHTML = "Tarcísio Alves"  //adicionando valor ao elemento
+pElement.innerHTML = "Desenvolvedor"
+spanElement.innerHTML = "Cataguases - MG"
+
+mainElement.style.display = 'flex'  //colocando estilo no mainElement
+mainElement.style.flexDirection = 'center'
+
+mainElement.classList.add('flex', 'flex-col', 'gap-4') //adicionando as classes do css
+
+h1Element.style.fontSize = '2.4rem'  
+pElement.style.fontSize = '1.8rem'
+spanElement.style.fontSize = '1.4rem'
+
+//criando os elemento posicionado em tela
+bodyElement.appendChild(mainElement)
+mainElement.appendChild(h1Element)
+mainElement.appendChild(pElement)
+mainElement.appendChild(spanElement)
+
+
+// console.log(body)
